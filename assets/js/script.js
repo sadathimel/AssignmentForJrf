238ddc3fc6508aed2e9217bfd6aee875d4a0406d
sadathimel <sadathossen.cse@gmail.com>
2024-10-03 23:28:14 +0600

    add animation part

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,3 +47,96 @@ function scrollToWhatwedo() {
 function scrollToDigikoein() {
   document.getElementById("Digikoein").scrollIntoView({ behavior: "smooth" });
 }
+
+// slider
+// var arrow_one = document.querySelector(".arrow_one");
+// var arrow_two = document.querySelector(".arrow_two");
+// arrow_one.addEventListener("click", function () {
+//   var lis = document.querySelectorAll(".carousel li");
+//   lis.forEach((li) => {
+//     if(li.value = 1){
+//       console.log(li.value)
+//       gsap.to(".card1",{x:"-400px"})
+//     }
+//   });
+// });
+
+const arrowOne = document.querySelector(".arrow_one");
+const arrowTwo = document.querySelector(".arrow_two");
+const lis = document.querySelectorAll(".carousel li");
+let currentIndex = 0;
+const totalItems = lis.length;
+
+// Handle left arrow click
+arrowOne.addEventListener("click", function () {
+  currentIndex = currentIndex > 0 ? currentIndex - 1 : totalItems - 1;
+  updateSlider();
+});
+
+// Handle right arrow click
+arrowTwo.addEventListener("click", function () {
+  currentIndex = currentIndex < totalItems - 1 ? currentIndex + 1 : 0;
+  updateSlider();
+});
+
+// Update slider position using GSAP
+function updateSlider() {
+  const targetX = -currentIndex * 400; // 400 is the width of each card
+  gsap.to(".carousel ul", {
+    x: targetX + "px",
+    duration: 0.5,
+    ease: "power2.out",
+  });
+  console.log(`Current Slide: ${currentIndex + 1}`);
+}
+
+var PaymentSystem = document.querySelector(".PaymentSystem");
+var CurrencyExchange = document.querySelector(".CurrencyExchange");
+var FoundTransfer = document.querySelector(".FoundTransfer");
+var PaymentSystem1 = document.querySelector(".PaymentSystem1");
+var CurrencyExchange1 = document.querySelector(".CurrencyExchange1");
+var FundTransfer1 = document.querySelector(".FundTransfer1");
+
+PaymentSystem.addEventListener("click", function () {
+  one();
+});
+CurrencyExchange.addEventListener("click", function () {
+  two();
+});
+FoundTransfer.addEventListener("click", function () {
+  three();
+});
+
+function one() {
+  PaymentSystem.classList.add("hidden");
+
+  CurrencyExchange.classList.remove("hidden");
+  FoundTransfer.classList.remove("hidden");
+
+  PaymentSystem1.classList.remove("hidden");
+
+  CurrencyExchange1.classList.add("hidden");
+  FundTransfer1.classList.add("hidden");
+}
+function two() {
+  PaymentSystem.classList.remove("hidden");
+
+  CurrencyExchange.classList.add("hidden");
+  FoundTransfer.classList.remove("hidden");
+
+  PaymentSystem1.classList.add("hidden");
+
+  CurrencyExchange1.classList.remove("hidden");
+  FundTransfer1.classList.add("hidden");
+}
+function three() {
+  PaymentSystem.classList.remove("hidden");
+
+  CurrencyExchange.classList.remove("hidden");
+  FoundTransfer.classList.add("hidden");
+
+  PaymentSystem1.classList.add("hidden");
+
+  CurrencyExchange1.classList.add("hidden");
+  FundTransfer1.classList.remove("hidden");
+}
